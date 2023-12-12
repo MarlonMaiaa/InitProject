3240818c610950172072b8e666531c1df19289df
Marlon Maia <92398630+MarlonMaiaa@users.noreply.github.com>
2023-12-12 13:17:44 -0300

    Atualização necessaria

--- src/main/java/com/mycompany/initproject/InitProject.java
+++ src/main/java/com/mycompany/initproject/InitProject.java
@@ -14,22 +14,22 @@ public class InitProject {
 
     public static void main(String[] args) {
    
-        int n1 , n2 , n3 , m;
+        float n1 , n2 , n3 , m;
         Scanner tc =  new Scanner (System.in);
         
-        System.out.println("Qual é a primeira nota do aluno :");
-        n1 = tc.nextInt();
+        System.out.println("Qual é a primeira nota do aluno: ");
+        n1 = tc.nextFloat();
         System.out.println("Qual e a segunda nota do aluno: ");
-        n2 = tc.nextInt();
+        n2 = tc.nextFloat();
         System.out.println("Qual e a terceira nota do aluno: ");
-        n3 = tc.nextInt();
+        n3 = tc.nextFloat();
         
        m = (n1+n2+n3)/3;
         System.out.println("A nota do aluno e: "+m);
         
-        if (m>=7) { System.out.println("O aluno esta aprovado");
+        if (m>=7) { System.out.println("O aluno esta aprovado com a nota "+ m);
         }
-        else { System.out.println("O aluno esta reprovado");
+        else { System.out.println("O aluno esta reprovado com a nota "+m);
             
         }
     }
